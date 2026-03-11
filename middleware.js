7278cb1c5c98404ae0d54044c95d5f96f01804df
[Olatunbosun Victor] <[olatunbosunvictor1@gmail.com]>
2026-03-11 01:00:58 +0100

    deployed frontend

--- middleware.js
+++ middleware.js
@@ -2,10 +2,16 @@ import { NextResponse } from "next/server";
 
 export function middleware(request) {
   const host = request.headers.get("host");
+  const { pathname } = request.nextUrl;
 
-  if (host === "youtube.joshspotmedia.com") {
+  // Only rewrite the homepage request
+  if (host === "youtube.joshspotmedia.com" && pathname === "/") {
     return NextResponse.rewrite(new URL("/start", request.url));
   }
 
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: "/:path*",
+};
